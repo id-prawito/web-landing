6fb14cbf89c69daac2ed8753c4a513cca5eade6b
Prawito <prawito.id@gmail.com>
2022-07-11 13:02:56 +0700

    setup structure folder

---
+++ src/app/App.js
@@ -0,0 +1,15 @@
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p style={{color: "#000"}}>
+          Web Landing
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
