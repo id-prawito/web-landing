5ca44b5ae30ecb948e62c97b026c1feb7ed9ae46
Prawito <prawito.id@gmail.com>
2022-07-23 19:08:58 +0700

    fix test page simulasi

--- src/pages/Simulasi.js
+++ src/pages/Simulasi.js
@@ -1,11 +1,17 @@
 import React from "react";
-import { FocusComponentColor } from "../components/atom";
+import { FocusComponentColor, ReactHelmet } from "../components/atom";
 import imgContact from "../assets/img/contact-us.png";
-import FormKpr from "../components/form";
+import { FormKpr } from "../components";
+import { useLocation } from "react-router-dom";
 
 const Simulasi = () => {
+  const location = useLocation();
   return (
     <>
+      <ReactHelmet
+        title="Bank Artha Mas Abadi | Simulasi Kredit"
+        url={location.pathname}
+      />
       <FocusComponentColor backgroundColor="#26ac42" img={imgContact} />
       <FormKpr />
     </>
